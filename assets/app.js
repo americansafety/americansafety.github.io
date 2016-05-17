afaa98712770019608c4980c23c8eb25afeb73dd
Rishka <aperlman@me.com>
2016-05-17 10:02:03 -0400

    added app.js

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+angular.module('snapbrim', ['ui.bootstrap', 'plunker', 'ngTouch', 'ngAnimate', 'ngSanitize'], function($httpProvider){
+
+        .controller('MainCtrl', MainCtrl)
+    function MainCtrl($scope, $http, $document, $uibModal, orderByFilter) {
+
+    });
+})();
